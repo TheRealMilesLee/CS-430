c803880cc297d665a5e5b9c43b160767c49e3ae9
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-10-06 15:59:29 -0500

    Update

--- Project/GroupProjectQueryMySQL/app/src/main/java/com/truman/edu/hengyi/groupprojectquerymysql/MainActivity.java
+++ Project/GroupProjectQueryMySQL/app/src/main/java/com/truman/edu/hengyi/groupprojectquerymysql/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-// Please see https://github.com/google/volley
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.*;
-import com.android.volley.toolbox.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,20 +21,17 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity
 {
-    String url = "https://sand.truman.edu/~hl3265/phpquery" +
-            ".php";
+    String url = "http://150.243.198.74:8080/phpquery" + ".php";
+
     Context mContext = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mContext = this.getApplicationContext();
-
-        /*
-         * This RequestQueue needs to be initialized in onCreate. Otherwise, you will get this:
-         * java.lang.NullPointerException: Attempt to invoke virtual method 'java.io.File android.content.Context.getCacheDir()' on a null object reference
-         */
         RequestQueue requestQueue = Volley.newRequestQueue(mContext);
         final TextView txt = (TextView) findViewById(R.id.txt);
 
@@ -42,48 +42,53 @@ public class MainActivity extends AppCompatActivity
          *  https://developer.android.com/training/volley/simple
          */
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>()
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+        Request.Method.GET, url, null,
+        new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+                // display response
+                Log.d("Response: ", response.toString());
+                try
                 {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // display response
-                        Log.d("Response: ", response.toString());
-                        try {
-                            JSONArray data = response.getJSONArray("data");
-                            for (int i = 0; i < data.length(); i++) {
-                                JSONObject jasondata = data.getJSONObject(i);
-
-                                // These are mapping to column names
-                                String name = jasondata.getString("NAME");
-                                String job = jasondata.getString("JOB");
-                                txt.append(name + " " + job + " " + " \n");
-                                // txt is the TextView
-
-                            }
-                            txt.append("--- End of Data ---\n"); // put the data to TextView
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                    JSONArray data = response.getJSONArray("data");
+                    for (int index = 0; index < data.length(); index++)
+                    {
+                        JSONObject JSONDataReceive = data.getJSONObject(index);
+                        Log.d("Test", JSONDataReceive.getString("Student_ID"));
+                        // These are mapping to column names
+                        String Student_ID = JSONDataReceive.getString(
+                                "Student_ID");
+                        String First_Name = JSONDataReceive.getString(
+                                "First_Name");
+                        String Last_Name = JSONDataReceive.getString(
+                                "Last_Name");
+                        txt.append(
+                                Student_ID + " " + First_Name + " " + Last_Name + " \n");
                     }
-                },
-                new Response.ErrorListener()
+                } catch (JSONException e)
                 {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Log.d("Error.Response", error.getMessage());
-                        error.printStackTrace();
-                    }
+                    e.printStackTrace();
                 }
-        );
+            }
+        },
+        new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                // Log.d("Error.Response", error.getMessage());
+                error.printStackTrace();
+            }
+        });
         requestQueue.add(jsonObjectRequest);
     }
-
     @Override
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         super.onDestroy();
         mContext = null;
     }
 }
-

84695c52a2774b5563e906541006f6d0094cfcde
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-10-06 20:52:27 -0500

    Update

--- Project/Question 3/phpquery.php
+++ Project/Question 3/phpquery.php
@@ -1,9 +1,4 @@
 <?php
-/**
-* Hengyi Li
-* This file is to delete words
-*/
-
 
 $host = '127.0.0.1'; //Address of your database
 $name = 'root'; // account to login to MysQL
@@ -12,7 +7,7 @@ $db = 'Team_Member'; //name of db
 $con = mysqli_connect($host, $name, $pwd) or die("connection failed");
 mysqli_select_db($con, $db) or die("db selection failed");
 
-$result = mysqli_query($con, "select TeamMember.Student_ID, TeamMember.First_Name, TeamMember.Last_Name, SecretPlace.Lat, SecretPlace.long_param as Long from TeamMember join SecretPlace where TeamMember.Student_ID = SecretPlace.Student_ID;");
+$result = mysqli_query($con, "select TeamMember.Student_ID, TeamMember.First_Name, TeamMember.Last_Name, SecretPlace.Lat, SecretPlace.long_param from TeamMember join SecretPlace where TeamMember.Student_ID = SecretPlace.Student_ID;");
 
 // Going to parse the result set
 while ($row = mysqli_fetch_assoc($result))

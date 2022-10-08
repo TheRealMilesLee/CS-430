09431c5280c2ba223953db9aba0a20fd91eb9097
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-10-08 10:46:52 -0500

    modified:   SpringBoot-Sample/complete/src/main/java/com/example/springboot/Application.java modified:   SpringBoot-Sample/complete/src/main/java/com/example/springboot/HelloController.java

--- SpringBoot-Sample/complete/src/main/java/com/example/springboot/Application.java
+++ SpringBoot-Sample/complete/src/main/java/com/example/springboot/Application.java
@@ -1,7 +1,8 @@
+/**
+ * It's a Spring Boot application that runs a command line runner
+ */
 package com.example.springboot;
-
 import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,23 +12,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
+  @Bean
+	// It's a Spring Boot application that runs a command line runner
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx)
+	{
+		return args ->
+		{
+      System.out.println("Let's inspect the beans provided by Spring Boot:");
+      String[] beanNames = ctx.getBeanDefinitionNames();
+      Arrays.sort(beanNames);
+			for (String beanName : beanNames)
+			{
+        System.out.println(beanName);
+      }
+    };
+  }
 }
--- SpringBoot-Sample/complete/src/main/java/com/example/springboot/HelloController.java
+++ SpringBoot-Sample/complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,10 +1,5 @@
-/*
- * @Author: TheRealMilesLee grandtheftauto710@outlook.com
- * @Date: 2022-06-30 11:53:40
- * @LastEditors: TheRealMilesLee grandtheftauto710@outlook.com
- * @LastEditTime: 2022-10-06 21:48:11
- * @FilePath: /SpringBoot-Sample/complete/src/main/java/com/example/springboot/HelloController.java
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+/**
+ * It's a Spring Boot controller that responds to HTTP requests at the root path ("/")
  */
 package com.example.springboot;
 

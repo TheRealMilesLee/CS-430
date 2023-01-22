2a4c7bb433fd80ea4c3945aa64bc2a077a60d7ba
Ideologist <hl3265@truman.edu>
2023-01-21 18:28:46 -0600

    更新 phpquery.php

--- Project/Question 3/phpquery.php
+++ Project/Question 3/phpquery.php
@@ -2,7 +2,7 @@
 
 $host = '127.0.0.1'; //Address of your database
 $name = 'root'; // account to login to MysQL
-$pwd = 'TheMilesLee710#'; //pwd
+$pwd = 
 $db = 'Team_Member'; //name of db
 $con = mysqli_connect($host, $name, $pwd) or die("connection failed");
 mysqli_select_db($con, $db) or die("db selection failed");
